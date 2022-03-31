56de3d0c66733b9db374edb0551f92f4265a7d14
unknown <ashah@softwareinstitute.com>
2022-03-31 18:04:12 +0100

    cleaning text

--- src/fib.cpp
+++ src/fib.cpp
@@ -6,5 +6,5 @@ int fib(int n)
     if (n == 0 || n == 1)
     return n;
   else
-    return (fib(n-1) + fib(n-2));
+    return (fib(n - 1) + fib(n - 2));
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,17 @@
 int main()
 {
     int n, m= 0, i;
-    std::cout<<"Enter the number of terms: "; 
-    std::cin>>n;
+    std::cout << "Enter the number of terms: "; 
+    std::cin >> n;
       //accepting the terms
-    std::cout<<"Fibonacci Series: ";
+    std::cout << "Fibonacci Series: ";
     for (i = 1; i <= n; i++)
     {
-        std::cout<<" "<<fib(m);
+        std::cout << " " << fib(m);
         m++;
     }
   
   return 0;
 
-  
+
 }
